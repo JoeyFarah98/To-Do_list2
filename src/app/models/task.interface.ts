307f522cc1b9c5d4960eafc8447018687e25eeb4
Joseph Farah <josephfarah@icloud.com>
2019-12-04 14:46:10 +1100

    added html and data service

---
+++ src/app/models/task.interface.ts
@@ -0,0 +1,5 @@
+export interface Task{
+    name:string;
+    created: number;
+    status: boolean;
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { timer, Subscription } from 'rxjs';
+import { Task } from '../Models/task.interface';
+import { DataService } from '../tabs/data.service';
+import { getLocaleDateTimeFormat } from '@angular/common';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: 'app-tracker',
+  templateUrl: './tracker.page.html',
+  styleUrls: ['./tracker.page.scss'],
 })
-export class Tab1Page {
+export class TrackerPage implements OnInit {
+  taskForm:FormGroup;
+  whencreated:number;
 
-  constructor() {}
+  constructor(
+    private formBuilder:FormBuilder,
+    private dataService:DataService
+  ) { }
 
+  ngOnInit() {
+    this.taskForm = this.formBuilder.group({
+     name: ['', [Validators.required, Validators.minLength(3) ] ] 
+    });
+  }
+
+
+  addTask() {
+    let task:Task = {
+      name: this.taskForm.get('name').value,
+      created: new Date().getTime(),
+      status: false
+
+    }
+    this.dataService.addToList( task );  
+    this.taskForm.reset();  
+  }
 }
